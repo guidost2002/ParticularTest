44ccf9faf0f54ba7db211b7a99ee41bab4d43980
guidost2002 <40010627+guidost2002@users.noreply.github.com>
2018-09-14 12:02:49 -0300

    Prueba database

--- Registro/index.js
+++ Registro/index.js
@@ -5,16 +5,12 @@ function register(){
 
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
-  var userRpass = document.getElementById("rpassword-field").value;
+  //var userRpass = document.getElementById("rpassword-field").value;
   var userName = document.getElementById("nombre_field").value;
   var userSurname = document.getElementById("apellido_field").value;
   var userPhone = document.getElementById("number_field").value;
-  if (userRpass !=userPass) {
-    window.alert("Las contraseñas no coinciden")
-  }
-  else {
+
       writeUserData(userName,userPass,userEmail,userPhone);
-  }
 
 
 
@@ -31,6 +27,15 @@ function register(){
   });
 
 }
+function traer(){
+  var userId = firebase.auth().currentUser.uid;
+return firebase.database().ref('/usuarios/' + userId).once('value').then(function(snapshot) {
+  var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+  // ...
+});
+window.alert(username);
+}
+
 function writeUserData(name,surname, email, phone) {
   firebase.database().ref('usuarios/').push({
     name: name,
