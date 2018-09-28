646019e63212e6836cd019ff9c2798dfb34443c2
malegre01 <40864392+malegre01@users.noreply.github.com>
2018-09-28 08:48:24 -0300

    TerminarLogin

--- Login/index.js
+++ Login/index.js
@@ -30,11 +30,15 @@ function login(){
 
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function(){
     var userId=firebase.auth().currentUser.uid;
-    return firebase.database().ref('/usuarios/' + userId).onAuthStateChanged('value').then(function(snapshot)
-    {
-      var nombree = (snapshot.val() && snapshot.val().name) || 'Anonymous';
-      var apellidoo = (snapshot.val() && snapshot.val().surname) || 'Anonymous';
-      alert("Bienvenido " + nombree +""+ apellidoo);
+    console.log(userId);
+    var ref = firebase.database().ref("usuarios/"+userId);
+  ref.once("value")
+    .then(function(snapshot) {
+      var name = snapshot.child("name").val(); // {first:"Ada",last:"Lovelace"}
+      var firstName = snapshot.child("name").val(); // "Ada"
+      var lastName = snapshot.child("surname").val(); // "Lovelace"
+      var age = snapshot.child("phone").val(); // null
+      alert(firstName+" "+lastName);
     });
 
 
